1b20cfc9e099979494b520fd64b8528481d37fd5
James Robiony-Rogers <43721451+JamesRobionyRogers@users.noreply.github.com>
2023-01-15 00:12:14 +1300

    Github GraphQL Pinned Repos Query Working
    - Implemented Github's GraphQL API to retrieve my pinned repos from
    
    - Need to make project card componenent

---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,100 @@
+import { useState, useEffect } from 'react'; 
+import React from 'react';
+
+const Projects = () => {
+
+	// eslint-disable-next-line no-unused-vars
+	const [pinnedRepos, setPinnedRepos] = useState({}); 
+
+	// Setting up the API to recieve from Github
+	const API_URL = "https://api.github.com/graphql"; 
+
+	const getPinnedRepos = async () => {
+		// Setting up request 
+		const accessToken = process.env.REACT_APP_GITHUB_ACCESS_TOKEN; 
+		const headers = { 
+			Authorization: `Bearer ${accessToken}`,
+			"Content-Type": "application/json"
+		}
+
+		// RepositoryOwner query is used to get the repository information 
+		// Then retrieving first 100 repositories, ordering them by updated_at, filtering by isFork, isLocked
+		// https://docs.github.com/en/graphql/reference/objects#repository 
+		const query = `{
+      		user(login: "JamesRobionyRogers") {
+        		pinnedItems(first: 6) {
+          			totalCount
+          			edges {
+            			node {
+              				... on Repository {
+                				name
+								description
+								homepageUrl
+								url
+
+								primaryLanguage {
+									color
+									name
+								}
+								languages(first: 100) {
+									edges {
+										node {
+											color
+											name
+										}
+									}
+								}
+                				updatedAt
+								createdAt
+              				}
+            			}
+          			}
+        		}
+      		}
+    	}`
+
+		// Sending the responce to Github API 
+		const responce = await fetch(API_URL, {
+			method: "POST", 
+			headers: headers, 
+			body: JSON.stringify({ query })
+		})
+
+		const data = await responce.json(); 
+
+		console.debug(data.data.user.pinnedItems);
+		
+		setPinnedRepos(data.data.user.pinnedItems); 
+
+	}
+
+	useEffect(() => {
+		getPinnedRepos(); 
+	}, [])
+
+	// Deconstructing the pinned repos 
+	// const totalRepos = pinnedRepos.totalCount; 
+	
+
+	return (
+		<div id="projects" className="mt-5">
+			<div className="position-relative">
+
+
+				<div className="container ">
+
+					<h4 className="card-title mb-4">
+						<span className="text-gradient">Projects</span>
+					</h4>
+
+					<div className="container d-flex flex-wrap justify-content-around">
+
+
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Projects;
