5bfc7139dc570bb7162be9aec813ee3795ffe518
James Robiony-Rogers <43721451+JamesRobionyRogers@users.noreply.github.com>
2023-01-18 18:44:11 +1300

    Add CodeCamp & Internships to experiences

--- src/sections/Experience/Experience.jsx
+++ src/sections/Experience/Experience.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaGraduationCap,  } from 'react-icons/fa'; 
+import { FaGraduationCap, FaRegIdBadge } from 'react-icons/fa'; 
 import { HiOutlineCode } from 'react-icons/hi';
 
 import './Experience.scss'; 
@@ -20,9 +20,25 @@ const experiences = [
 		title: "University of Otago",
 		description: "I am currently studying towards a Bachelor of Applied Science (BASc), Majoring in Software Engineering, Minoring in Computer Science",
 		extendedDescription: "",
-		date: "2022 - 2024",
+		date: "2022 - 2024", 
 		location: "",
+		icon: () => <FaGraduationCap /> // "" // may want to do this by
+	},
+	{
+		title: "CodeCamp",
+		description: "While working with CodeCamp I have helped run a range of programming classes targeted towards primary and intermediate aged students. During these classes I am often going round helping and facilitating groups with their projects and providing one on one help with students when they get stuck.",
+		extendedDescription: "",
+		date: "Nov 2022 - Feb 2023",
+		location: "Wellington Region, New Zealand",
 		icon: () => <HiOutlineCode />
+	}, 
+	{
+		title: "Seeking Experience Through an Internship", 
+		description: "Motivated to make the most of my summer break, I have been approaching Wellington based companies in the tech indestry to see if they would be willing to take me on as an intern.",
+		extendedDescription: "I am looking for a company that is willing to take me on for an internship ending mid February, as that is when I will be heading back to university. Looking back at the work I have done already, approaching people with regards to a job offer was something I was not comfortable with, but have consistantly put myseld out there and drastricly imporved my communication skills as a result.",
+		date: "Currently", 
+		location: "", 
+		icon: () => <FaRegIdBadge />
 	}
 ]
 
@@ -43,7 +59,6 @@ const Experience = () => {
 					<h4 className="card-title mb-4">
 						<span className="text-gradient">Education & Experience</span>
 					</h4>
-
 
 					{
 						/* Map over the experience array returning TimelineEvent components  */
