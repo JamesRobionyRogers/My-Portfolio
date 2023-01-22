33260a2a8f1851b2b6fbed24f858f6a4edc4586d
James Robiony-Rogers <43721451+JamesRobionyRogers@users.noreply.github.com>
2023-01-22 20:02:32 +1300

    Add CV Preview using SweetAlert2
    Added an onClick feature to the preview image nested in the card.
    
    SweetAlert displays the three pages of my CV within the popup
    
    Added SweetAlert styling to the SCSS file as well

--- src/sections/PreviewCV/PreviewCV.jsx
+++ src/sections/PreviewCV/PreviewCV.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 
 import CVCardPreview from '../../assets/CV_JamesRobiony-Rogers.pdf'; 
+import Page1 from '../../assets/cv-pages/CV_JamesRobiony-Rogers_Page1.pdf';
+import Page2 from '../../assets/cv-pages/CV_JamesRobiony-Rogers_Page2.pdf';
+import Page3 from '../../assets/cv-pages/CV_JamesRobiony-Rogers_Page3.pdf'; 
 
 import './PreviewCV.scss'
 
 
 const PreviewCV = () => {
+
+    const handlePreviewClick = () => {
+        // Setting up SweetAlert2
+        const MySwal = withReactContent(Swal);
+
+        MySwal.fire({
+            title: 'CV Preview',
+            html:
+            `<div class="cv-preview_alert-container"> 
+                <img src=${Page1} alt="CV Page 1">
+                <img src=${Page2} alt="CV Page 2">
+                <img src=${Page3} alt="CV Page 3">
+            </div>`, // style="overflow-y: scroll; height:400px; width: 100%;"
+
+            showConfirmButton: false,
+            showCloseButton: true,
+        });
+    }
+
     return (
         <div id="cv" className="mt-5 pt-5 pb-5">
             <div className="card me-5 ms-5 text-bg-gray border border-light overflow-hidden" style={{marginTop: "-1rem"}}>
@@ -18,7 +42,7 @@ const PreviewCV = () => {
                         {/* <!-- Content --> */}
 
                         <div className="cv-preview card-shadow position-absolute">
-                            <img className="img-thumbnail mw-10" src={CVCardPreview} alt="Preview of James Robiony-Rogers CV" />
+                            <img className="img-thumbnail mw-10" src={CVCardPreview} alt="Preview of James Robiony-Rogers CV" onClick={ handlePreviewClick } />
                         </div>
                     </div>
 
