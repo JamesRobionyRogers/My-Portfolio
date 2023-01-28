450cda8ece017db9b12c62fa6770dadbc81f7c48
James Robiony-Rogers <43721451+JamesRobionyRogers@users.noreply.github.com>
2023-01-28 15:30:32 +1300

    Add `StickyLinks` component
    Adding the new `StickyLinks` componet to the app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import './App.css'; 
 
 import Header from './sections/Header/Header.jsx'; 
+import StickyLinks from './components/StickyLinks.jsx';
 import Skills from './sections/Skills/Skills.jsx';
 import Projects from './sections/Projects/Projects.jsx';
 import Experience from './sections/Experience/Experience';
@@ -13,6 +14,7 @@ function App() {
   	return (
 		<div className="App">
 			<Header /> 
+			<StickyLinks />
 			<Skills />
 			<Projects /> 
 			<Experience /> 
