6057d4024b95ac09e7f9e8261b6b72b399de5666
James Robiony-Rogers <43721451+JamesRobionyRogers@users.noreply.github.com>
2023-01-28 15:29:36 +1300

    Create `StickyLinks` component with styling
    Migrated the sticky links to it's own component
    
    Applied styling to it as well

---
+++ src/components/StickyLinks.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { SiGithub, SiLinkedin } from 'react-icons/si'
+
+import './StickyLinks.scss'; 
+
+const StickyLinks = () => {
+    return (
+        <div class="sticky-links text-bg-dark">
+            <a class="sticky-link sticky-link-github" href="https://github.com/JamesRobionyRogers">
+                <i className="sticky-link-github"><SiGithub /></i>
+            </a>
+
+            <a class="sticky-link sticky-link-linkedin" href="https://www.linkedin.com/in/JamesRobionyRogers/">
+                <SiLinkedin />
+            </a>
+        </div>
+    );
+}
+
+export default StickyLinks;
